5217961a5fc0c6a2ff028cbdb09bbd7b00b8dbe6
Tihomir13 <tihomir13n2002@abv.bg>
2024-06-30 10:03:03 +0300

    update favicon and UX on Desktop Hourly tab Also SEO Optimization

--- src/app/components/left/weather-info/weather-hours/weather-hours.component.ts
+++ src/app/components/left/weather-info/weather-hours/weather-hours.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
 import { DatePipe } from '@angular/common';
 
 @Component({
@@ -6,8 +6,57 @@ import { DatePipe } from '@angular/common';
   standalone: true,
   imports: [DatePipe],
   templateUrl: './weather-hours.component.html',
-  styleUrl: './weather-hours.component.css',
+  styleUrls: ['./weather-hours.component.css'],
 })
-export class WeatherHoursComponent {
+export class WeatherHoursComponent implements AfterViewInit {
   @Input() weatherHourly?: any;
+
+  @ViewChild('weatherHourlyContainer') weatherHourlyContainer!: ElementRef;
+
+  ngAfterViewInit() {
+    this.addMouseDragFunctionality();
+  }
+
+  addMouseDragFunctionality() {
+    const weatherHourly = this.weatherHourlyContainer.nativeElement;
+
+    let isDown = false;
+    let startX: number;
+    let scrollLeft: number;
+
+    weatherHourly.addEventListener('mousedown', (e: MouseEvent) => {
+      isDown = true;
+      weatherHourly.classList.add('active');
+      startX = e.pageX - weatherHourly.offsetLeft;
+      scrollLeft = weatherHourly.scrollLeft;
+    });
+
+    weatherHourly.addEventListener('mouseleave', () => {
+      isDown = false;
+      weatherHourly.classList.remove('active');
+    });
+
+    weatherHourly.addEventListener('mouseup', () => {
+      isDown = false;
+      weatherHourly.classList.remove('active');
+    });
+
+    weatherHourly.addEventListener('mousemove', (e: MouseEvent) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - weatherHourly.offsetLeft;
+      const walk = (x - startX) * 1.5; // Adjust the multiplier as needed
+      weatherHourly.scrollLeft = scrollLeft - walk;
+    });
+
+    // Enable wheel scroll functionality
+    weatherHourly.addEventListener('wheel', (e: WheelEvent) => {
+      e.preventDefault();
+      weatherHourly.scrollLeft += e.deltaY;
+    });
+  }
+
+  trackByTimestamp(index: number, weatherHour: any): number {
+    return weatherHour.timestamp;
+  }
 }
